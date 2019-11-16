e1fa8978f9f87ef860c8434ac235e0d6df6d2266
ScripteJunkie <syntaxstux@gmail.com>
2019-11-16 11:00:02 -0500

    Fixes?

--- src/main.cpp
+++ src/main.cpp
@@ -62,8 +62,9 @@
 // Motor12              motor         12              
 // ---- END VEXCODE CONFIGURED DEVICES ----
 #include "vex.h"
-#include "autonomous.h"
+//#include "autonomous.h"
 using namespace vex;
+vex::competition Competition;
  
  // gloabal variables
 int count = 0;
@@ -75,10 +76,23 @@ int count = 0;
 
  };
 
-// define your global instances of motors and other devices here
- int main() {
-   autonomous();
-     while (true) {
+ void pre_auton( void ) {
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+  
+}
+
+
+void autonomous( void ) {
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+  
+}
+
+
+
+void usercontrol( void ) {
+  while (true) {
         motor_L1.spin(directionType::fwd, (Controller1.Axis4.value() + Controller1.Axis3.value())/2, velocityUnits::pct); //(Axis3+Axis4)/2;
  	      motor_R1.spin(directionType::fwd, (Controller1.Axis4.value() - Controller1.Axis3.value())/2, velocityUnits::pct);//(Axis3-Axis4)/2;
         motor_L2.spin(directionType::fwd, (Controller1.Axis4.value() + Controller1.Axis3.value())/2, velocityUnits::pct); //(Axis3+Axis4)/2;
@@ -92,7 +106,7 @@ int count = 0;
           motor_liftL.setBrake(brakeType::brake);
         }
                 if (Controller1.ButtonL1.pressing()) {
-          motor_liftL.spin(directionType::fwd, -5, velocityUnits::pct);
+          motor_liftL.spin(directionType::rev, 5, velocityUnits::pct);
           task::sleep(30);
         }
         else {
@@ -110,6 +124,25 @@ int count = 0;
         task::sleep(10);
          //disp();
      }  
+       if (Controller1.ButtonUp.pressing()) {
+          motorliftR.spin(directionType::fwd, 5, velocityUnits::pct);
+        }
+      if (Controller1.ButtonDown.pressing()) {
+          motor_liftR.spin(directionType::rev, 5, velocityUnits::pct);
+        }
+}
+
+// define your global instances of motors and other devices here
+ int main() {
+   //autonomous();
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  pre_auton();
+       
+    //Prevent main from exiting with an infinite loop.                        
+    while(1) {
+      vex::task::sleep(100);//Sleep the task for a short amount of time to prevent wasted resources.
+}
+
 };
-
-
--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -4,6 +4,7 @@ using namespace vex;
 using signature = vision::signature;
 using code = vision::code;
 
+
 // A global instance of brain used for printing to the V5 Brain screen
 brain  Brain;
 
@@ -13,7 +14,8 @@ motor motor_R2 = motor(PORT10, ratio18_1, false);
 motor motor_L1 = motor(PORT11, ratio18_1, false);
 motor motor_L2 = motor(PORT20, ratio18_1, false);
 controller Controller1 = controller(primary);
-motor motor_liftL = motor(PORT12, ratio18_1, false);
+motor motorliftL = motor(PORT12, ratio18_1, false);
+motor motorliftR = motor(PORT13, ratio18_1, false);
 motor IntakeL = motor(PORT2, ratio18_1, false);
 motor IntakeR = motor(PORT3, ratio18_1, false);
 
