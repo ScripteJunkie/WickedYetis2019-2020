9e4dc6630d927c0af7a2547451f36f9ecfd3533d
Ashton <30474771+ScripteJunkie@users.noreply.github.com>
2019-11-24 15:57:52 -0500

    Working Now!

--- include/autonomous.h
+++ include/autonomous.h
@@ -1,23 +1,33 @@
 #include "autonomous-config.h"
-
+#include <iomanip> 
+#include <iostream> 
+#include <stdlib.h> 
+#include <unistd.h>
+using namespace std;
 void autonomousboiii( void ) {
   #include "visionconfig.h"
   
-int i = 0;
+  int seconds = 0;
   //driveForward( 2 * 12 ); // drive forwards
   //driveForward(-2 * 12 ); // drive backwards
-  while (i < 500) {
-    motor_L1.spin(directionType::fwd, 20, velocityUnits::pct);
-    motor_R1.spin(directionType::fwd, -20, velocityUnits::pct);
-    motor_L2.spin(directionType::fwd, 20, velocityUnits::pct); 
-    motor_R2.spin(directionType::fwd, -20, velocityUnits::pct);
-    i++;
-      if (Limit.pressing() == true) {
-        motor_L1.setBrake(brakeType::brake);
-        motor_L2.setBrake(brakeType::brake);
-        motor_R1.setBrake(brakeType::brake);
-        motor_R2.setBrake(brakeType::brake);
-      }
-    task::sleep(100);
-  }
-}
+ // while (true){
+    motor_L1.spin(directionType::fwd, -100, velocityUnits::pct);
+    motor_R1.spin(directionType::fwd, 100, velocityUnits::pct);
+    motor_L2.spin(directionType::fwd, -100, velocityUnits::pct); 
+    motor_R2.spin(directionType::fwd, 100, velocityUnits::pct);
+    vex::task::sleep(4000);
+    motor_L1.spin(directionType::rev, -100, velocityUnits::pct);
+    motor_R1.spin(directionType::rev, 100, velocityUnits::pct);
+    motor_L2.spin(directionType::rev, -100, velocityUnits::pct); 
+    motor_R2.spin(directionType::rev, 100, velocityUnits::pct);
+    vex::task::sleep(1000);
+    //sleep(1);
+   // seconds++;
+    if (seconds < 10) {
+      motor_L1.stop(vex::brakeType::coast);
+      motor_R1.stop(vex::brakeType::coast);
+      motor_L2.stop(vex::brakeType::coast); 
+      motor_R2.stop(vex::brakeType::coast);
+    }
+  };
+//}
--- src/main.cpp
+++ src/main.cpp
@@ -1,241 +1,13 @@
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_L1             motor         11              
-// motor_L2             motor         20              
-// IntakeL              motor         2               
-// IntakeR              motor         3               
-// motor_liftL          motor         12              
-// motor_liftR          motor         14              
-// Ranged               sonar         A, B            
-// Limit                limit         C               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_L1             motor         11              
-// motor_L2             motor         20              
-// IntakeL              motor         2               
-// IntakeR              motor         3               
-// motor_liftL          motor         12              
-// motor_liftR          motor         14              
-// Ranged               sonar         A, B            
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_L1             motor         11              
-// motor_L2             motor         20              
-// IntakeL              motor         2               
-// IntakeR              motor         3               
-// motor_liftL          motor         12              
-// motor_liftR          motor         14              
-// Ranged               sonar         A, B            
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_L1             motor         11              
-// motor_L2             motor         20              
-// IntakeL              motor         2               
-// Intake3              motor         3               
-// motor_liftL          motor         12              
-// motor_liftR          motor         14              
-// Ranged               sonar         A, B            
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_L1             motor         11              
-// motor_L2             motor         20              
-// IntakeL              motor         2               
-// Intake3              motor         3               
-// motor_liftL          motor         12              
-// motor_liftR          motor         14              
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_L1             motor         11              
-// motor_L2             motor         20              
-// IntakeL              motor         2               
-// Intake3              motor         3               
-// motor_liftL          motor         12              
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_L1             motor         11              
-// motor_L2             motor         20              
-// IntakeL              motor         2               
-// Intake3              motor         3               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_L1             motor         11              
-// motor_L2             motor         20              
-// IntakeL              motor         2               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_L1             motor         11              
-// motor_L2             motor         20              
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_L1             motor         11              
-// motor_20             motor         20              
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_11             motor         11              
-// motor_20             motor         20              
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_11             motor         11              
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// motor_R1             motor         1               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// RangeFind            sonar         A, B            
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_L1             motor         11              
-// motor_L2             motor         20              
-// Controller1          controller                    
-// motor_liftL          motor         12              
-// IntakeL              motor         2               
-// IntakeR              motor         3               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_L1             motor         11              
-// motor_L2             motor         20              
-// Controller1          controller                    
-// motor_liftL          motor         12              
-// IntakeL              motor         2               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_L1             motor         11              
-// motor_L2             motor         20              
-// Controller1          controller                    
-// motor_liftL          motor         12              
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_L1             motor         11              
-// motor_L2             motor         20              
-// Controller1          controller                    
-// Motor_12             motor         12              
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_L1             motor         11              
-// motor_L2             motor         20              
-// Controller1          controller                    
-// Motor_12             motor         12              
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// motor_R1             motor         1               
-// motor_R2             motor         10              
-// motor_L1             motor         11              
-// motor_L2             motor         20              
-// Controller1          controller                    
-// Motor12              motor         12              
-// ---- END VEXCODE CONFIGURED DEVICES ----
 #include "vex.h"
 #include "autonomous.h"
 #include "robot-config.h"
 
 using namespace vex;
 vex::competition Competition;
+
  
  // gloabal variables
-int count = 0;
+//int count = 0;
 
 // functions go here
  void disp() {
@@ -271,45 +43,49 @@ void usercontrol( void ) {
 
         if (Controller1.ButtonL2.pressing()) {
           motor_liftL.spin(directionType::fwd, 5, velocityUnits::pct);
-          task::sleep(30);
+          task::sleep(20);
         }
         else {
-          motor_liftL.setBrake(brakeType::brake);
+          motor_liftL.stop(vex::brakeType::brake);
         }
-                if (Controller1.ButtonL1.pressing()) {
+
+        if (Controller1.ButtonL1.pressing()) {
           motor_liftL.spin(directionType::rev, 5, velocityUnits::pct);
-          task::sleep(30);
+          task::sleep(20);
         }
         else {
-          motor_liftL.setBrake(brakeType::brake);
+          motor_liftL.stop(vex::brakeType::brake);
         }
         
         if (Controller1.ButtonR2.pressing()) {
           IntakeL.spin(directionType::fwd, 100, velocityUnits::pct);
           IntakeR.spin(directionType::rev, 100, velocityUnits::pct);
+          task::sleep(20);
         }
         else {
-          IntakeL.setBrake(brakeType::coast);
-          IntakeR.setBrake(brakeType::coast);
+          IntakeL.stop(vex::brakeType::coast);
+          IntakeR.stop(vex::brakeType::coast);
+
         }
 
         if (Controller1.ButtonR1.pressing()) {
           IntakeL.spin(directionType::rev, 100, velocityUnits::pct);
           IntakeR.spin(directionType::fwd, 100, velocityUnits::pct);
+          task::sleep(20);
         }
         else {
-          IntakeL.setBrake(brakeType::coast);
-          IntakeR.setBrake(brakeType::coast);
+          IntakeL.stop(vex::brakeType::coast);
+          IntakeR.stop(vex::brakeType::coast);
         }
 
-        if (Controller1.ButtonUp.pressing()) {
-          motor_liftL.spin(directionType::rev, 5, velocityUnits::pct);
-        }
-        else if (Controller1.ButtonDown.pressing()) {
-          motor_liftL.spin(directionType::fwd, 5, velocityUnits::pct);
-        }
+       // if (Controller1.ButtonUp.pressing()) {
+         // motor_liftL.spin(directionType::rev, 5, velocityUnits::pct);
+       // }
+       // else if (Controller1.ButtonDown.pressing()) {
+        //  motor_liftL.spin(directionType::fwd, 5, velocityUnits::pct);
+       // }
         // waits 10ms befroe update and allow other tasks to run
-        task::sleep(10);
+        //task::sleep(10);
          //disp();
      } 
 }
